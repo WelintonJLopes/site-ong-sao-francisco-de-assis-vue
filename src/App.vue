a80b14c0c7c2bb61c033f5c44d1bc547601a9e1d
WelintonJLopes <welintonj.lopes@hotmail.com>
2022-06-12 14:10:45 -0300

    Projeto criado, estrutura base.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-	<div id="app">
-		<router-view />
-	</div>
+	<v-app>
+		<v-main>
+			<Home />
+		</v-main>
+	</v-app>
 </template>
 
+<script>
+import Home from "./components/Home.vue";
 
+export default {
+	name: "App",
+	components: { Home },
+	data: () => ({}),
+};
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+	<v-container>Home page</v-container>
+</template>
+
+<script>
+export default {
+	name: "Home",
+};
+</script>
+
+<style scoped>
+</style>
